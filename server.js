7a5df8b3ec8f33b6cd66a46ee48dcd6298de45f1
Ederdal <degarcruz2004@gmail.com>
2025-03-14 19:08:06 -0600

    Subiendo la base de datos

--- server.js
+++ server.js
@@ -37,11 +37,11 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Conexión a la base de datos
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "edgar2411",
-  database: process.env.DB_NAME || "hospital_general_8a_idgs_220219",
-  port: process.env.DB_PORT || 3309,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
